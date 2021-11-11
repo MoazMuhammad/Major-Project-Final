872ae0d73958bec6260ae368eac865518678a4a4
MoazMuhammad <93631424+MoazMuhammad@users.noreply.github.com>
2021-11-11 12:00:39 +1100

    Update Program.cs

--- Major Project Final/Program.cs
+++ Major Project Final/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace Major_Project_Final
+namespace Major_Project
 {
     /// <summary>
     /// ASCIIGame starter code. 
@@ -38,6 +38,9 @@ namespace Major_Project_Final
     {
         static void Main(string[] args)
         {
+            //To see what I am standing on
+            char tile;
+            
             // Player Inventory List
             List<Items> Inventory = new List<Items>();
 
@@ -45,13 +48,14 @@ namespace Major_Project_Final
             List<Items> ChestStorage = new List<Items>();
             ChestStorage.Add(new HealingPotion("Healing Potion"));
             ChestStorage.Add(new HealingPotion("Healing Potion"));
-                
+
             // Enemies List
             List<Enemy> Enemies = new List<Enemy>();
 
             // Interactables Lists
             // Door Lists
             List<Door> Doors = new List<Door>();
+            
             // Chest Lists
             List<Chest> Chests = new List<Chest>();
 
@@ -71,11 +75,22 @@ namespace Major_Project_Final
             // Enemies
             Enemy Enemy1 = new Enemy(50, 8, 3, Enemies, 20);
 
+            // Doors
+            Door door1 = new Door(new ClosedDoor(), 3, 5, Doors);
+            Door door2 = new Door(new ClosedDoor(), 9, 2, Doors);
+            Door door3 = new Door(new ClosedDoor(), 19, 4, Doors);
+            Door door4 = new Door(new ClosedDoor(), 49, 7, Doors);
+            Door door5 = new Door(new ClosedDoor(), 56, 11, Doors);
+            Door door6 = new Door(new ClosedDoor(), 16, 13, Doors);
+            Door door7 = new Door(new ClosedDoor(), 32, 17, Doors);
+            Door door8 = new Door(new ClosedDoor(), 43, 24, Doors);
+            Door door9 = new Door(new ClosedDoor(), 22, 32, Doors);
+
             //This path will read a map from your project directory//
             // School
-            // var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, @"\\nas125e2.actedu.net.au\studenthome\born2004\09\0083844\my documents\visual studio 2015\Projects\Major Project\Major Project\Map.txt");
+            var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, @"\\nas125e2.actedu.net.au\studenthome\Born2004\09\0083844\My Documents\Visual Studio 2015\Projects\Major Project\Major Project\Map.txt");
             // Home
-            var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, @"C:\Users\PC\OneDrive\Desktop\Canberra College\Programming\Assignments\Major Project Final\Major Project Final\map.txt");
+            //var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, @"C:\Users\PC\OneDrive\Desktop\Canberra College\Programming\Assignments\Major Project Final\Major Project Final\map.txt");
             string[] map = File.ReadAllLines(path);
 
             ConsoleKeyInfo user_input;
@@ -94,10 +109,49 @@ namespace Major_Project_Final
                     for (int x = 0; x < map[y].Length; x++)
                     {
                         // Render Doors
-                        if (map[y][x] == '+')
+                        if (x == door1.doorX && y == door1.doorY)
                         {
-                            Door d = new Door(new ClosedDoor(), x, y, Doors);
-                            d.Draw();
+                            door1.Draw();
+                        }
+
+                        else if (x == door2.doorX && y == door2.doorY)
+                        {
+                            door2.Draw();
+                        }
+
+                        else if (x == door3.doorX && y == door3.doorY)
+                        {
+                            door3.Draw();
+                        }
+
+                        else if (x == door4.doorX && y == door4.doorY)
+                        {
+                            door4.Draw();
+                        }
+
+                        else if (x == door5.doorX && y == door5.doorY)
+                        {
+                            door5.Draw();
+                        }
+
+                        else if (x == door6.doorX && y == door6.doorY)
+                        {
+                            door6.Draw();
+                        }
+
+                        else if (x == door7.doorX && y == door7.doorY)
+                        {
+                            door7.Draw();
+                        }
+
+                        else if (x == door8.doorX && y == door8.doorY)
+                        {
+                            door8.Draw();
+                        }
+
+                        else if (x == door9.doorX && y == door9.doorY)
+                        {
+                            door9.Draw();
                         }
 
                         // Render Chests
@@ -137,9 +191,13 @@ namespace Major_Project_Final
                     e.AI(Player, map);
                 }
 
+                // What is under neath me? 
+                tile = map[Player.playerY][Player.playerX];
+                Console.Write($"You are standing on {tile}");
+
                 // Capture user input
                 user_input = Console.ReadKey();
-                
+
                 // Inventory
                 if (user_input.Key == ConsoleKey.I)
                 {
@@ -154,22 +212,12 @@ namespace Major_Project_Final
                     {
                         Player.playerY--;
                     }
-
-                    else if (map[Player.playerY - 1][Player.playerX] != '-')
-                    {
-                        Player.playerY--;
-                    }
                 }
 
                 // Move Down
                 else if (user_input.Key == ConsoleKey.DownArrow)
                 {
                     if (map[Player.playerY + 1][Player.playerX] != '#' && map[Player.playerY + 1][Player.playerX] != 'E' && map[Player.playerY + 1][Player.playerX] != '+' && map[Player.playerY + 1][Player.playerX] != '~')
-                    {
-                        Player.playerY++;
-                    }
-
-                    else if (map[Player.playerY + 1][Player.playerX] != '-')
                     {
                         Player.playerY++;
                     }
@@ -182,11 +230,6 @@ namespace Major_Project_Final
                     {
                         Player.playerX++;
                     }
-
-                    else if (map[Player.playerY][Player.playerX + 1] != '-')
-                    {
-                        Player.playerX--;
-                    }
                 }
 
                 // Move Left
@@ -197,27 +240,12 @@ namespace Major_Project_Final
                     {
                         Player.playerX--;
                     }
-
-                    else if (map[Player.playerY][Player.playerX - 1] != '-')
-                    {
-                        Player.playerX--;
-                    }
                 }
 
                 // Interaction with Doors
                 foreach (Door d in Doors)
                 {
-                    // If Player is South of Door
-                    if (map[Player.playerY - 1][Player.playerX] == map[d.doorY][d.doorX])
-                    {
-                        d.Request1();
-                    }
-
-                    // If Player is North of Door
-                    else if (map[Player.playerY + 1][Player.playerX] == map[d.doorY][d.doorX])
-                    {
-                        d.Request1();
-                    }
+                    d.ToggleDoor(Player, map, user_input);
                 }
 
                 // Intercation with Chests
